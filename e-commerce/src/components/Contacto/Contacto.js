7a4f54e126e4db0471ae85221f792135dab4e703
itsiaraaar <businessiara47@gmail.com>
2022-12-04 12:39:52 -0300

    Agregue el botón de whatsapp
    Botón de bootstrap de whatsapp con link al chat de whatsapp

--- e-commerce/src/components/Contacto/Contacto.js
+++ e-commerce/src/components/Contacto/Contacto.js
@@ -4,12 +4,16 @@ import '../Contacto/Contacto.css'
 
 function Contacto() {
   return (
+    <>
     <div className='div'>
       <h1 className='title'>Contacto</h1>
-    {/* "https://www.facebook.com/madrenaturaesencias/" */}
+      {/* "https://www.facebook.com/madrenaturaesencias/" */}
       <FormContact />
     </div>
-
+    <div>
+    {/* <a aria-label="Chat on WhatsApp" href="https://wa.me/2945696137"> <img alt="Chat on WhatsApp" src="WhatsAppButtonGreenLarge.svg" /> </a>
+ */}    </div>
+    </>
   )
 }
 
--- e-commerce/src/components/footer/Footer.js
+++ e-commerce/src/components/footer/Footer.js
@@ -1,59 +1,70 @@
 import {
-    MDBFooter,
-    MDBContainer,
-    MDBIcon,
-    MDBBtn
-  } from 'mdb-react-ui-kit';
-  import "../footer/Footer.css";
-  export default function Footer() {
-    return (
-      <MDBFooter id="footer" className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
-        <MDBContainer className='pt-4'>
-          <section className='mb-4'>
-            <MDBBtn
-              rippleColor="dark"
-              color='link'
-              floating
-              size="lg"
-              className='text-dark m-1'
-              href='https://www.facebook.com/madrenaturaesencias/'
-              role='button'
-            >
-              <MDBIcon fab className='fab fa-facebook-f' />
-            </MDBBtn>
-  
-            <MDBBtn
-              rippleColor="dark"
-              color='link'
-              floating
-              size="lg"
-              className='text-dark m-1'
-              href='https://www.instagram.com/madrenaturaesencias/'
-              role='button'
-            >
-              <MDBIcon fab className='fa-instagram' />
-            </MDBBtn>
-  
-            <MDBBtn
-              rippleColor="dark"
-              color='link'
-              floating
-              size="lg"
-              className='text-dark m-1'
-              href='https://github.com/itsiaraaar'
-              role='button'
-            >
-              <MDBIcon fab className='fa-github' />
-            </MDBBtn>
-          </section>
-        </MDBContainer>
-  
-        <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
-          © 2020 Copyright:
-          <a className='text-dark' href='https://mdbootstrap.com/'>
-            MDBootstrap.com
-          </a>
-        </div>
-      </MDBFooter>
-    );
-  }
+  MDBFooter,
+  MDBContainer,
+  MDBIcon,
+  MDBBtn
+} from 'mdb-react-ui-kit';
+import "../footer/Footer.css";
+export default function Footer() {
+  return (
+    <MDBFooter id="footer" className='text-center text-white' style={{ backgroundColor: '#f1f1f1' }}>
+      <MDBContainer className='pt-4'>
+        <section className='mb-4'>
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="lg"
+            className='text-dark m-1'
+            href='https://www.facebook.com/madrenaturaesencias/'
+            role='button'
+          >
+            <MDBIcon fab className='fab fa-facebook-f' />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="lg"
+            className='text-dark m-1'
+            href='https://www.instagram.com/madrenaturaesencias/'
+            role='button'
+          >
+            <MDBIcon fab className='fa-instagram' />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="lg"
+            className='text-dark m-1'
+            href='https://github.com/itsiaraaar'
+            role='button'
+          >
+            <MDBIcon fab className='fa-github' />
+          </MDBBtn>
+
+          <MDBBtn
+            rippleColor="dark"
+            color='link'
+            floating
+            size="lg"
+            className='text-dark m-1'
+            href='https://wa.me/2945525129'
+            role='button'
+          >
+            <MDBIcon fab className='fa-whatsapp' />
+          {/* <img alt="Chat on WhatsApp" src="../footer/whatsapp-svgrepo-com.svg"/> */}
+          </MDBBtn>
+        </section>
+      </MDBContainer>
+
+      <div className='text-center text-dark p-3' style={{ backgroundColor: 'rgba(0, 0, 0, 0.2)' }}>
+        © 2022 Copyright:
+        <a className='text-dark' href='https://mdbootstrap.com/'> mdbootstrap</a>
+      </div>
+    </MDBFooter>
+  );
+}
